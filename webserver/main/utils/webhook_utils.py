dad51ea3a9c48ba48fcce7e8531a76ef463277ec
adityapatil123 <ad1tya.patil.3297@gmail.com>
2022-07-11 18:41:17 +0530

    Add error-handling for sse webhook

--- webserver/main/utils/webhook_utils.py
+++ webserver/main/utils/webhook_utils.py
@@ -6,6 +6,9 @@ from main.logger.custom_logging import log
 
 def post_count_response_to_client(route, payload):
     client_webhook_endpoint = get_config_by_name('CLIENT_WEBHOOK_ENDPOINT')
-    response = requests.post(f"{client_webhook_endpoint}/{route}", json=payload)
-    status_code = response.status_code
+    try:
+        response = requests.post(f"{client_webhook_endpoint}/{route}", json=payload)
+        status_code = response.status_code
+    except requests.exceptions.ConnectionError:
+        status_code = 500
     log(f"Got {status_code} for {payload} on {route}")
