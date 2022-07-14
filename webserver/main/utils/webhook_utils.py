257293836c2944c66963ef4fb48d238385460da5
adityapatil123 <ad1tya.patil.3297@gmail.com>
2022-07-14 16:08:55 +0530

    Add retries for sse webhook if failed

--- webserver/main/utils/webhook_utils.py
+++ webserver/main/utils/webhook_utils.py
@@ -1,14 +1,25 @@
 import requests
+from retry import retry
 
 from main.config import get_config_by_name
 from main.logger.custom_logging import log
 
 
+@retry(tries=3, delay=1)
+def requests_post(endpoint, json):
+    response = requests.post(endpoint, json=json)
+    status_code = response.status_code
+    if status_code != 200:
+        raise requests.exceptions.HTTPError("Request Failed!")
+    return status_code
+
+
 def post_count_response_to_client(route, payload):
     client_webhook_endpoint = get_config_by_name('CLIENT_WEBHOOK_ENDPOINT')
     try:
-        response = requests.post(f"{client_webhook_endpoint}/{route}", json=payload)
-        status_code = response.status_code
+        status_code = requests_post(f"{client_webhook_endpoint}/{route}", json=payload)
+    except requests.exceptions.HTTPError:
+        status_code = 400
     except requests.exceptions.ConnectionError:
         status_code = 500
     log(f"Got {status_code} for {payload} on {route}")
