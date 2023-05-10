75231f15a02bd227465f03edb18a83cc52284df6
Sahil Sharma <123928721+sahilsharma9@users.noreply.github.com>
2023-05-10 18:36:13 +0530

    fix: removed commented code

--- webserver/main/utils/webhook_utils.py
+++ webserver/main/utils/webhook_utils.py
@@ -23,7 +23,6 @@ def MeasureTime(f):
             elapsed = timeit.default_timer() - start_time
             if gcold:
                 gc.enable()
-            print('[{}]Function "{}": {}s'.format(datetime.datetime.now(), f.__name__, elapsed))
         return result
 
     return _wrapper
@@ -39,7 +38,6 @@ def requests_post_with_retries(url, payload, headers=None):
 
 
 def requests_post(url, raw_data, headers=None):
-    print("url", url)
     response = requests.post(url, data=raw_data, headers=headers)
     return response.text, response.status_code
 
@@ -47,10 +45,9 @@ def requests_post(url, raw_data, headers=None):
 @MeasureTime
 def post_count_response_to_client(route, payload):
     client_webhook_endpoint = get_config_by_name('CLIENT_WEBHOOK_ENDPOINT')
-    print(route, "issue" in route)
     if "issue" in route:
-        client_webhook_endpoint = client_webhook_endpoint.replace("clientApi", "issueApi")
-    print(client_webhook_endpoint)
+        client_webhook_endpoint = client_webhook_endpoint.replace(
+            "clientApi", "issueApi")
     try:
         status_code = requests_post_with_retries(
             f"{client_webhook_endpoint}/{route}", payload=payload)
@@ -63,14 +60,13 @@ def post_count_response_to_client(route, payload):
     log(f"Got {status_code} for {payload} on {route}")
     return status_code
 
+
 @MeasureTime
 def post_on_bg_or_bpp(url, payload, headers={}):
     log(f"Making POST call for {payload['context']['message_id']} on {url}")
     headers.update({'Content-Type': 'application/json'})
     raw_data = json.dumps(payload, separators=(',', ':'))
-    print("Raw data", raw_data)
     response_text, status_code = requests_post(url, raw_data, headers=headers)
-    print("response_text", response_text, "status_code", status_code)
     return json.loads(response_text), status_code
 
 
