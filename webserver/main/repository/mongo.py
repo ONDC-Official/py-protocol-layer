35d13b7e9f8aed627d9afb2da351dffb095ee029
Aditya Patil <ad1tya.patil.3297@gmail.com>
2023-09-17 23:03:33 +0530

    Remove exclusion fields for on_search_items

--- webserver/main/repository/mongo.py
+++ webserver/main/repository/mongo.py
@@ -60,8 +60,7 @@ def collection_find_all(mongo_collection, query_object, sort_field=None, sort_or
     try:
         log(f"Getting entries from collection {mongo_collection.name}")
         if mongo_collection.name == "on_search_items":
-            catalogue_objects = mongo_collection.find(query_object, {"categories": 0, "providers": 0, "locations": 0,
-                                                                     "fulfillments": 0, "context": 0})
+            catalogue_objects = mongo_collection.find(query_object, {})
         else:
             catalogue_objects = mongo_collection.find(query_object)
         if sort_field:
@@ -103,8 +102,7 @@ def collection_find_distinct(mongo_collection, query_object, distinct=None):
 @MeasureTime
 def collection_find_one(mongo_collection, query_object):
     if mongo_collection.name == "on_search_items":
-        catalog = mongo_collection.find_one(query_object, {"categories": 0, "providers": 0, "locations": 0,
-                                                           "fulfillments": 0})
+        catalog = mongo_collection.find_one(query_object, {})
     else:
         catalog = mongo_collection.find_one(query_object)
     if catalog:
