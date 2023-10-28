7a7369c34f3fc106c3f7a59cf0a4afb8ff6a62eb
Aditya Patil <ad1tya.patil.3297@gmail.com>
2023-10-28 20:06:44 +0530

    Update rabbitmq auto_ack=True

--- webserver/main/utils/rabbitmq_utils.py
+++ webserver/main/utils/rabbitmq_utils.py
@@ -1,9 +1,9 @@
 import functools
 import threading
-
+import time
 
 import pika
-
+from concurrent.futures import ThreadPoolExecutor
 
 from main.config import get_config_by_name
 from main.logger.custom_logging import log, log_error
@@ -50,35 +50,24 @@ def publish_message_to_queue(channel, exchange, routing_key, body, properties=No
 
 
 def consume_message(connection, channel, queue_name, consume_fn):
-    def callback(ch, delivery_tag, body):
-        try:
-            channel.basic_ack(delivery_tag)
-            log(f"Ack message {body} !")
-        except:
-            log_error(f"Something went wrong for {body} !")
 
     def do_work(delivery_tag, body):
         thread_id = threading.get_ident()
         log(f'Thread id: {thread_id} Delivery tag: {delivery_tag} Message body: {body}')
-        cb = functools.partial(callback, channel, delivery_tag, body)
 
         try:
             consume_fn(body)
         except Exception as e:
             log_error(f"Error processing message {body}: {e}")
 
-        connection.add_callback_threadsafe(cb)
-
     def on_message(ch, method_frame, header_frame, body):
         delivery_tag = method_frame.delivery_tag
-        t = threading.Thread(target=do_work, args=(delivery_tag, body))
-        t.start()
-        threads.append(t)
+        executor.submit(do_work, delivery_tag, body)
 
-    threads = []
+    executor = ThreadPoolExecutor(max_workers=get_config_by_name('CONSUMER_MAX_WORKERS', 10))
     on_message_callback = functools.partial(on_message)
 
-    channel.basic_consume(queue=queue_name, on_message_callback=on_message_callback, auto_ack=False)
+    channel.basic_consume(queue=queue_name, on_message_callback=on_message_callback, auto_ack=True)
     log('Waiting for messages:')
 
     try:
@@ -86,8 +75,6 @@ def consume_message(connection, channel, queue_name, consume_fn):
     except KeyboardInterrupt:
         channel.stop_consuming()
 
-    # Wait for all to complete
-    for thread in threads:
-        thread.join()
-
+    # Wait for all threads to complete
+    executor.shutdown()
     connection.close()
