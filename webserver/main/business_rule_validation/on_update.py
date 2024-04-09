dbae45e9518b219e68292abe184dfc2bf4de1b0f
Aditya Patil <ad1tya.patil.3297@gmail.com>
2024-04-09 23:14:02 +0530

    Update schema and add business rule validations

---
+++ webserver/main/business_rule_validation/on_update.py
@@ -0,0 +1,11 @@
+from main.business_rule_validation.common import validate_sum_of_quote_breakup
+from main.models.error import BaseError
+from main.repository.ack_response import get_ack_response
+
+
+def validate_business_rules_for_on_confirm(payload):
+    error = validate_sum_of_quote_breakup(payload)
+    if error:
+        return get_ack_response(context=payload["context"], ack=False,
+                                error={"type": BaseError.JSON_SCHEMA_ERROR.value, "code": "20000",
+                                       "message": error}), 400
