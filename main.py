891b168ce11906701e996a30c68d277ffb11d178
Aditya Patil <ad1tya.patil.3297@gmail.com>
2024-06-10 11:18:18 +0530

    Add requirements.txt

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ from routes import api
 
 
 def create_app(config_name):
-    app = Flask(__name__)
-    app.config.from_object(config.config_by_name[config_name])
-    app.app_context().push()
-    api.init_app(app)
+    flask_app = Flask(__name__)
+    flask_app.config.from_object(config.config_by_name[config_name])
+    flask_app.app_context().push()
+    api.init_app(flask_app)
     init_database()
-    CORS(app)
-    return app
+    CORS(flask_app)
+    return flask_app
+
+
+app = create_app(os.getenv("ENV", "dev"))
 
 
 if __name__ == '__main__':
-    created_app = create_app(os.getenv("ENV", "dev"))
-    created_app.run(host="0.0.0.0", port=created_app.config["PORT"])
+    app.run(host="0.0.0.0", port=app.config["PORT"])
