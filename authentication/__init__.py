c2ca401b40c245f9cce33a207f867809d90dccfd
Aditya Patil <ad1tya.patil.3297@gmail.com>
2024-06-18 14:00:26 +0530

    Update authentication considering app and request type

--- authentication/__init__.py
+++ authentication/__init__.py
@@ -9,10 +9,17 @@ from services.request_dump import dump_request_payload_with_response
 
 def authenticate(func):
     def wrapper(*args, **kwargs):
-        if get_config_by_name("VERIFICATION_ENABLE"):
+        payload = request.get_json()
+        action = payload["context"]["action"]
+        app_type = get_config_by_name("TYPE").split("_")[-1]
+        if app_type == "BAP":
+            auth_enable = "on_" in action
+        else:
+            auth_enable = "on_" not in action
+
+        if auth_enable and get_config_by_name("VERIFICATION_ENABLE"):
             resp = authenticate_request(request.data, request.headers)
             if resp is not None:
-                payload = request.get_json()
                 status_code = 401
                 dump_request_payload_with_response(payload, dict(request.headers), resp, status_code=status_code)
                 return resp, status_code
