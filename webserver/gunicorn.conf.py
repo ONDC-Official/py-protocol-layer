02b2d37941d16b8f535547c29d6e6f03fda5a67d
Aditya Patil <ad1tya.patil.3297@gmail.com>
2024-10-11 16:09:18 +0530

    Remove unnessary helth check gunicorn logs

--- webserver/gunicorn.conf.py
+++ webserver/gunicorn.conf.py
@@ -1,14 +1,50 @@
-import logging
+import os
+from logging import Filter
 
-workers = 8
-bind = "0.0.0.0:5555"
-timeout = 300
 
-# Logging setup
-loglevel = 'info'
-accesslog = '-'  # Log access logs to stdout
-errorlog = '-'   # Log error logs to stdout
+# Health check log filter
+class IgnoreHealthCheckFilter(Filter):
+    def filter(self, record):
+        if "GET / " in record.getMessage():
+            return False
+        return True
 
-# Example of configuring logging with a specific handler
-logging.basicConfig(level=logging.INFO,
-                    format='%(process)d %(thread)d %(asctime)s [%(name)s][%(levelname)s]::%(message)s')
+
+# Gunicorn configuration
+workers = 2  # Number of worker processes
+threads = 4  # Number of threads per worker
+timeout = 600  # Worker timeout in seconds
+
+# Fetch PORT from environment variable, default to 8000 if not set
+port = os.getenv('PORT', '8000')
+bind = f"0.0.0.0:{port}"
+
+
+# Logging configuration
+logconfig_dict = {
+    'version': 1,
+    'filters': {
+        'ignore_health_check': {
+            '()': IgnoreHealthCheckFilter,
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'access',
+            'filters': ['ignore_health_check'],
+        },
+    },
+    'loggers': {
+        'gunicorn.access': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+    'formatters': {
+        'access': {
+            'format': '%(process)d %(thread)d %(asctime)s [%(name)s][%(levelname)s]::%(message)s',
+        },
+    },
+}
