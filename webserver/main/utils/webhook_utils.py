2cdcd74d1508cb629dc83b1caa871eaebadafa05
Aditya Patil <ad1tya.patil.3297@gmail.com>
2024-10-24 18:13:16 +0530

    Add NO Dashboard logs

--- webserver/main/utils/webhook_utils.py
+++ webserver/main/utils/webhook_utils.py
@@ -167,6 +167,8 @@ def make_request_to_no_dashboard(payload, response=False):
         "data": payload
     }
 
+    log(f"Updated payload for NO Dashboard: {updated_payload}")
+
     try:
         token = get_config_by_name("NO_DASHBOARD_BEARER_TOKEN")
         status_code = requests.post(f"{dashboard_webhook_endpoint}/v1/api/push-txn-logs",
