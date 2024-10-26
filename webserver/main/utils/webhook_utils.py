b401ec5e57cde60c7fa030d16124f999684e7904
Aditya Patil <ad1tya.patil.3297@gmail.com>
2024-10-26 10:23:52 +0530

    Add endpoint in log

--- webserver/main/utils/webhook_utils.py
+++ webserver/main/utils/webhook_utils.py
@@ -168,6 +168,7 @@ def make_request_to_no_dashboard(payload, response=False):
 
     try:
         token = get_config_by_name("NO_DASHBOARD_BEARER_TOKEN")
+        log(f"Making request on {dashboard_webhook_endpoint}/v1/api/push-txn-logs")
         log(f"Logging request payload: {json.dumps(updated_payload)}")
         status_code = requests.post(f"{dashboard_webhook_endpoint}/v1/api/push-txn-logs",
                                     headers={"Authorization": f"Bearer {token}",
