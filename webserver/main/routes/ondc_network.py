8fb83e9c1e39b44f1b7af8492e187462309be550
Aditya Patil <ad1tya.patil.3297@gmail.com>
2024-11-07 20:30:30 +0530

    Integrate new queue: nack-message-queue

--- webserver/main/routes/ondc_network.py
+++ webserver/main/routes/ondc_network.py
@@ -7,7 +7,8 @@ from main.config import get_config_by_name
 from main.logger.custom_logging import log
 from main.models.catalog import SearchType
 from main.repository.ack_response import get_ack_response
-from main.service import send_message_to_queue_for_given_request, send_message_to_elastic_search_queue
+from main.service import send_message_to_queue_for_given_request, send_message_to_elastic_search_queue, \
+    send_message_to_nack_message_queue
 from main.service.common import add_bpp_response, dump_request_payload, update_dumped_request_with_response
 from main.service.search import dump_on_search_payload
 from main.service.utils import validate_auth_header, dump_validation_failure_request
@@ -44,7 +45,15 @@ class GatewayOnSearch(Resource):
                 send_message_to_elastic_search_queue(message) if get_config_by_name('ELASTIC_SEARCH_QUEUE_ENABLE') else None
             return get_ack_response(request_payload[constant.CONTEXT], ack=True), 200
         else:
-            dump_validation_failure_request(request_payload, resp[0]["error"]["message"])
+            doc_id = dump_validation_failure_request(request_payload, resp[0]["error"]["message"])
+            if request_type == SearchType.FULL.value:
+                # Nack message forwarding so that it can be forwarded to Kafka
+                message = {
+                    "doc_id": str(doc_id),
+                    "request_type": request_type,
+                    "error_type": "VALIDATION_FAILURE"
+                }
+                send_message_to_nack_message_queue(message) if get_config_by_name('NACK_MESSAGE_QUEUE_ENABLE') else None
             return resp
 
 
